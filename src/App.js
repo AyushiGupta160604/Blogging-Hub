5ef574298f3107a663391c7cb4ca799e6a095813
Ayushi Gupta <133566741+AyushiGupta160604@users.noreply.github.com>
2024-06-20 02:20:53 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NotFound from './NotFound';
 
 function App() {
-  // const title = "Welcome to the blogging section!"
-  // const likes = 50;
-  // const person = {name: "John", age: 18}; We can't pass objects dynamically
-  // const bool = false; We can't pass booleans dynamically [it won't be displayed]
-  // const link = "http://www.google.com";
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        {/* <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-        <p>{ 10 }</p>
-        <p>{ "hello, world!" }</p>
-        <p>{ [1,2,3,4,5] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-        <a href = { link }>Google Site</a> */}
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />              
+            </Route>
+            <Route path="/create">
+              <Create />              
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />              
+            </Route>
+            <Route path="*">
+              <NotFound />              
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>  
   );
 }
 

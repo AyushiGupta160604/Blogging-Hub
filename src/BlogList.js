c0589a86b8f2eab4d30aa17976a1efd1c0d2df62
Ayushi Gupta <133566741+AyushiGupta160604@users.noreply.github.com>
2024-06-20 02:21:36 +0530

    Updated BlogList.js

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,4 @@
-// const BlogList = (props) => {
-//     const blogs = props.blogs;
-//     const title = props.title;
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const BlogList = ({ blogs, title }) => {
 
     return (
@@ -8,8 +6,10 @@ const BlogList = ({ blogs, title }) => {
             <h2>{ title }</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key = {blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
+                    <Link to={`/blogs/${blog.id}`}>
+                        <h2>{ blog.title }</h2>
+                        <p>Written by { blog.author }</p>
+                    </Link>
                 </div>
             ))}
         </div>

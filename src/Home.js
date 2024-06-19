bd9c716a8331c044ee2e169ec3c017c26ac0c7f2
Ayushi Gupta <133566741+AyushiGupta160604@users.noreply.github.com>
2024-06-20 02:24:04 +0530

    Updated Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,8 @@
-// import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 import useFetch from './useFetch';
 
 const Home = () => {
     const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
-    // We don't directly invoke the function because it just fires it even one button is not clicked, hence we wrap it in an anonymous function.
-    // const handleClickAgain = (name) => {
-    //     alert(`Hello ${name}!`);
-    // };
     return (
         <div className="home">
             { error && <div>{ error }</div> }

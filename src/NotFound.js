d9fd9893154294ce298a0cc9ce4a03dbc5bdcd80
Ayushi Gupta <133566741+AyushiGupta160604@users.noreply.github.com>
2024-06-20 02:26:56 +0530

    Created NotFound.js
    for any arbitrary url routes

---
+++ src/NotFound.js
@@ -0,0 +1,12 @@
+const NotFound = () => {
+    return (
+       <div className="not-found">
+            <h2>Sorry</h2>
+            <br/>
+            <br/>
+            <p>That page cannot be found</p>
+       </div> 
+    );
+}
+ 
+export default NotFound;
